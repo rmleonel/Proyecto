7a51ac8b3c67eb18b2f1ed7d32688f770a03df36
Leonel Romero <leoromero0412@gmail.com>
2023-12-30 00:18:29 +0000

    agregando formulario

--- src/proyecto1/appProyecto/urls.py
+++ src/proyecto1/appProyecto/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from appProyecto.views import profesores , estudiantes, cursos, entregables, index
+from appProyecto.views import (profesores , estudiantes, cursos, entregables, index, formulario)
 
 urlpatterns = [
     path("profesores/", profesores, name ='profesores'),
     path("estudiantes/", estudiantes, name = 'estudiantes'),
     path("cursos/", cursos, name = 'cursos'),
     path("entregables/", entregables, name = 'entregables'),
+    path("formulario/", formulario, name = 'formulario'),
     path("", index, name = 'index'),
 ]
--- src/proyecto1/appProyecto/views.py
+++ src/proyecto1/appProyecto/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from appProyecto.models import Profesor
 
+from appProyecto.models import Profesor, Curso
+
 
 def leer_profesor(request):
     profesor = Profesor(nombre = "Marcos", apellido = "Leguizamon", email= "mlegui@gmail.com")
@@ -32,3 +34,25 @@ def cursos(request):
 
 def entregables(request):
     return render(request, 'entregables.html')
+
+def formulario(request):
+
+    #Path de la ruta
+    print(f"path: {request.path}")
+    print(f"full path: {request.get_full_path()}")
+    print(f"host: {request.get_host()}")
+    print(f"si es segura: {request.is_secure()}")
+    print(f"metodo: {request.method}")
+    ua = request.META.get("HTTP_USER_AGENT")
+    print(f"ua: {ua}")
+
+    if request.method == "POST":
+
+        nombre = request.POST.get("curso")
+        camada = request.POST.get("camada")
+
+        curso = Curso(nombre=nombre, camada=camada)
+        curso.save()
+        return render(request,'index.html')
+
+    return render(request, "formulario.html")
